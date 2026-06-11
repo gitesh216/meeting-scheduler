25b784af9810e212c03d60402f59ede1693319b7
Gitesh <giteshzope19@gmail.com>
2026-06-11 21:48:36 +0530

    feat: integrate Prisma for database management and add user model

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import { app } from './app.js';
+import { connectDatabase } from './config/database.js';
 import { PORT } from './config/env.js';
 
 async function startServer() {
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
+        await connectDatabase();
         console.log(`[server]: Running on port ${PORT}`)
     });
 }
