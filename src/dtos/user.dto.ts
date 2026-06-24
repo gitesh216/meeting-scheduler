5a249823101fc6bb4d080488eba7bc9f4374941e
Gitesh <giteshzope19@gmail.com>
2026-06-24 18:53:38 +0530

    feat: add password validation to createUserSchema

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -7,6 +7,8 @@ export const createUserSchema = z.object({
         .string()
         .min(2, "Name must be at least 2 characters long")
         .max(50, "Name must be at most 50 characters long"),
+
+    password: z.string().min(6, "Password must be at least 6 characters long"),
 });
 
 export type CreateUserDto = z.infer<typeof createUserSchema>;
