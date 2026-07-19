757ce4bc9281e46eec060c8993bf80c2f37929e4
Gitesh <giteshzope19@gmail.com>
2026-07-19 22:31:58 +0530

    feat: add booking controller with create and list endpoints

---
+++ src/controllers/booking.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { ListHostBookingsQuery } from "../dtos/booking.dto.js";
+import {
+    createBookingPessimistically,
+    listHostBookings as listHostBookingsService,
+} from "../services/booking.service.js";
+import { sendSuccess } from "../utils/api-response.js";
+
+export async function create(req: Request, res: Response) {
+    const result = await createBookingPessimistically(req.userId, req.body);
+    sendSuccess(res, result, 201, "Booking created successfully");
+}
+
+export async function list(req: Request, res: Response) {
+    const query: ListHostBookingsQuery = req.query;
+    const result = await listHostBookingsService(req.userId, query);
+    sendSuccess(res, result);
+}
