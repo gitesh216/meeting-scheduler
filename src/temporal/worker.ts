4c1cac8234e1debae660fb3ad8786e551078765c
Gitesh <giteshzope19@gmail.com>
2026-08-10 15:36:42 +0530

    feat: add Kubernetes configurations for Temporal and worker deployments

--- src/temporal/worker.ts
+++ src/temporal/worker.ts
@@ -2,6 +2,9 @@ import { NativeConnection, Worker } from "@temporalio/worker";
 import { TEMPORAL_ADDRESS, TEMPORAL_TASK_QUEUE, TEMPORAL_NAMESPACE } from "../config/env.js";
 import * as activities from "./activities/index.js";
 import { fileURLToPath } from "node:url";
+import { extname } from "node:path";
+
+const workflowsExtension = extname(fileURLToPath(import.meta.url));
 
 async function run() {
     const connection = await NativeConnection.connect({
@@ -13,7 +16,7 @@ async function run() {
         namespace: TEMPORAL_NAMESPACE,
         taskQueue: TEMPORAL_TASK_QUEUE,
         activities,
-        workflowsPath: fileURLToPath(new URL("./workflows/index.ts", import.meta.url)),
+        workflowsPath: fileURLToPath(new URL(`./workflows/index${workflowsExtension}`, import.meta.url)),
     });
     console.log("[Temporal] Worker started for task queue", TEMPORAL_TASK_QUEUE);
     
